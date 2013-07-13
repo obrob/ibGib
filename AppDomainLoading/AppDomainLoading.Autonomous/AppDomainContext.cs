81725d6093a6112a487c4d9d96dbf883269e595c
Robert Raiford <rob@ibgib.com>
2013-07-13 19:53:04 -0400

    - got AppDomainContext instantiating correctly from the 2nd AppDomain and am calling Foo() successfully. - added some Debug.WriteLine() tracing to better follow what was happening. - added a 10 minute timeout to the WCF binding so I could take my time debugging.

--- AppDomainLoading/AppDomainLoading.Autonomous/AppDomainContext.cs
+++ AppDomainLoading/AppDomainLoading.Autonomous/AppDomainContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,32 @@ namespace AppDomainLoading.Autonomous
   [Serializable]
   public class AppDomainContext : MarshalByRefObject
   {
-    public void Foo()
+    public AppDomainContext()
     {
-      MyStringProperty = "Foo done did.";
+      Debug.WriteLine("\r\n---------- [AppDomainContext() constructor]");
+
+      this.MyCtorSetProperty = "I'm alive!";
+
+      Debug.WriteLine("------------- [AppDomainContext() constructor] this: " + this);
     }
 
-    public string MyStringProperty { get; set; }
+    public void Foo()
+    {
+      Debug.WriteLine("\r\n---------- [Foo()]");
+      
+      this.MyFooSetProperty = "Foo done did.";
+
+      Debug.WriteLine("------------- [Foo()] this: " + this);
+    }
+
+    public string MyCtorSetProperty { get; set; }
+    public string MyFooSetProperty { get; set; }
+
+    public override string ToString()
+    {
+      return string.Format("MyCtorSetProperty=\"{0}\", MyFooSetProperty=\"{1}\"",
+        this.MyCtorSetProperty ?? "<null>",
+        this.MyFooSetProperty ?? "<null>");
+    }
   }
 }
--- AppDomainLoading/AppDomainLoading.Autonomous/ServiceManager.cs
+++ AppDomainLoading/AppDomainLoading.Autonomous/ServiceManager.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.Remoting;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,31 +14,91 @@ namespace AppDomainLoading.Autonomous
   {
     public void LoadAppDomain()
     {
-      //var appDomainName = AutonomousResources.AppDomainNamePrefix +
-      //                        DateTime.Now.ToLongTimeString() +
-      //                        RandomPicker.Ton.PickLetters(Configuration.DefaultContextRandomSalt) +
-      //                        AutonomousResources.AppDomainNameSuffix;
-      var appDomainName = "blahblahdomain";
+      try
+      {
+        var adCurrent = AppDomain.CurrentDomain;
 
-      AppDomainSetup ads = new AppDomainSetup();
-      ads.ApplicationBase = System.Environment.CurrentDirectory;
-      //ads.ApplicationBase = AppDomain.CurrentDomain.BaseDirectory;
-      //ads.PrivateBinPath = AppDomain.CurrentDomain.BaseDirectory;
-      ads.DisallowBindingRedirects = false;
-      ads.DisallowCodeDownload = true;
-      ads.ConfigurationFile =
-          AppDomain.CurrentDomain.SetupInformation.ConfigurationFile;
+        LogAppDomain(adCurrent, "Current");
 
-      AppDomain ad2 = AppDomain.CreateDomain(appDomainName, null);
-      var contextType = typeof(AppDomainContext);
-      var assembly = contextType.Assembly.FullName;
-      ad2.AssemblyResolve += ad2_AssemblyResolve;
-      var blah2 = ad2.CreateInstanceFromAndUnwrap(assembly,
-                                                      typeof(AppDomainContext).FullName);
+        //var appDomainName = AutonomousResources.AppDomainNamePrefix +
+        //                        DateTime.Now.ToLongTimeString() +
+        //                        RandomPicker.Ton.PickLetters(Configuration.DefaultContextRandomSalt) +
+        //                        AutonomousResources.AppDomainNameSuffix;
+        var ad2Name = "blahblahdomain";
+
+        var adSetup = new AppDomainSetup()
+        {
+          //ApplicationBase = System.Environment.CurrentDirectory,
+          ApplicationBase = adCurrent.BaseDirectory,
+          //PrivateBinPath = adCurrent.BaseDirectory,
+          PrivateBinPath = Path.Combine(adCurrent.BaseDirectory, "bin"),
+          DisallowBindingRedirects = false,
+          DisallowCodeDownload = true,
+          ConfigurationFile = adCurrent.SetupInformation.ConfigurationFile
+        };
+
+        LogAppDomainSetup(adSetup, "ad2");
+
+        //var ad2 = AppDomain.CreateDomain(ad2Name, null);
+        var ad2 = AppDomain.CreateDomain(ad2Name, null, adSetup);
+
+        LogAppDomain(ad2, "ad2");
+
+        var contextType = typeof(AppDomainContext);
+        var assembly = contextType.Assembly; //.FullName;
+
+        //ad2.AssemblyResolve += ad2_AssemblyResolve;
+
+        //var blah2 = ad2.CreateInstanceFromAndUnwrap(
+        //  //assembly.FullName,
+        //  assembly.Location,
+        //  contextType.FullName);
+        //var foo = (AppDomainContext)blah2;
+
+        Debug.WriteLine(string.Format("-------------- Creating instance from assembly \"{0}\" of type \"{1}\"", assembly.FullName, contextType.FullName));
+        var obj = ad2.CreateInstance(assembly.FullName, contextType.FullName);
+        var adContext = (AppDomainContext)obj.Unwrap();
+
+        Debug.WriteLine("---------- [LoadAppDomain()] adContext after creation: " + adContext);
+
+        adContext.Foo();
+        Debug.WriteLine("---------- [LoadAppDomain()] adContext after Foo(): " + adContext);
+      }
+      catch (Exception ex)
+      {
+        Debug.WriteLine("[ LoadAppDomain() ] EXCEPTION: " + ex);
+        throw;
+      }
     }
+
+    private void LogAppDomainSetup(AppDomainSetup adSetup, string context)
+    {
+      Debug.WriteLine(System.Environment.NewLine);
+      Debug.WriteLine(string.Format("---------- [AppDomainSetup  \"{0}\"] ----------", context));
+      Debug.WriteLine(string.Format("------------- ApplicationName = \"{0}\"", adSetup.ApplicationName ?? "<null>"));
+      Debug.WriteLine(string.Format("------------- ApplicationBase = \"{0}\"", adSetup.ApplicationBase ?? "<null>"));
+      Debug.WriteLine(string.Format("------------- DisallowBindingRedirects = \"{0}\"", adSetup.DisallowBindingRedirects));
+      Debug.WriteLine(string.Format("------------- DisallowCodeDownload = \"{0}\"", adSetup.DisallowCodeDownload));
+    }
+
+    private void LogAppDomain(AppDomain ad, string context)
+    {
+      Debug.WriteLine(System.Environment.NewLine);
+      Debug.WriteLine(string.Format("---------- [AppDomain  \"{0}\"] ----------", context));
+      Debug.WriteLine(string.Format("------------- FriendlyName = \"{0}\"", ad.FriendlyName ?? "<null>"));
+      Debug.WriteLine(string.Format("------------- BaseDirectory = \"{0}\"", ad.BaseDirectory ?? "<null>"));
+      Debug.WriteLine(string.Format("------------- RelativeSearchPath = \"{0}\"", ad.RelativeSearchPath ?? "<null>"));
+      Debug.WriteLine(string.Format("------------- PrivateBinDirectory = \"{0}\\{1}\"", 
+        ad.BaseDirectory ?? "<BaseDirectory=null>", 
+        ad.RelativeSearchPath ?? "<RelativeSearchPath=null>"));
+    }  
 
     System.Reflection.Assembly ad2_AssemblyResolve(object sender, ResolveEventArgs args)
     {
+      Debug.WriteLine("---------- [ad2_AssemblyResolve()]");
+      Debug.WriteLine(string.Format("------------- args.Name = \"{0}\"", args.Name));
+      Debug.WriteLine(string.Format("------------- args.RequestingAssembly = \"{0}\"", args.RequestingAssembly.FullName));
+
       throw new NotImplementedException();
     }
   }
